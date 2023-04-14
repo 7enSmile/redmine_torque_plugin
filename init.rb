b12752b266194eda3d6b6a8f182b7d72fe75489d
Pankratov Semen <68625468+7enSmile@users.noreply.github.com>
2023-04-14 11:38:20 +0000

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_torque_plugin do
+  name 'Redmine Torque Plugin plugin'
+  author 'Semen Pankratov'
+  description 'This plugin is for integrating torque and redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'https://github.com/7enSmile'
+end
