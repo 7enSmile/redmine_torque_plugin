22021f44f919d14f2ae116b65b5920ecd1c7e06c
Pankratov Semen <68625468+7enSmile@users.noreply.github.com>
2023-04-17 13:12:30 +0000

    add description

--- app/controllers/webhooks_torque_controller.rb
+++ app/controllers/webhooks_torque_controller.rb
@@ -6,7 +6,12 @@ class WebhooksTorqueController < ApplicationController
     issue.project = Project.find_by_id(Setting.plugin_redmine_torque_plugin[:torque_project])
     issue.assigned_to = User.find_by_id(Setting.plugin_redmine_torque_plugin[:torque_user_assignee])
     issue.subject = data["job_id"]
-    issue.description = data["job_name"]
+    issue.description = "job info\n
+                         job_id: #{data["job_id"]} \n
+                         job_name: #{data["job_name"]} \n
+                         job_owner: #{data["job_owner"]} \n
+                         queue: #{data["queue"]}\n
+                         "
     issue.status = IssueStatus.find_by_name("New")
     issue.priority = IssuePriority.find_by_name("Normal")
     issue.author = User.find_by_id(1)
